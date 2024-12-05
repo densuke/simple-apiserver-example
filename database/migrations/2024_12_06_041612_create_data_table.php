32c73654620ec9bd694aad245cb989fc95175fcc
SATO Daisuke <densuke@fuga.jp>
2024-12-05 19:25:18 +0000

    テーブルの追加

--- database/migrations/2024_12_06_041612_create_data_table.php
+++ database/migrations/2024_12_06_041612_create_data_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('data', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->integer('count'); // 1〜10の整数
         });
     }
 
